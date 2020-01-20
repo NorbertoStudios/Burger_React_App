56586a0e4d17211fe7399ef36a738464eca01d06
Norberto Fernandez <Norberto@Norbertos-MacBook-Pro.local>
2020-01-20 01:32:32 -0500

    Add the ingredients Dynamically using the states

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Creating A Dynamic React App
+
+<H1>BURGER BUILDER</H1>
--- burgerbuilder/src/components/Burger/Burger.js
+++ burgerbuilder/src/components/Burger/Burger.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import classes from './Burger.module.css'
-import BurgerIngridient from './Burger-Ingridient/BurgerIngridients'
+import classes from "./Burger.module.css";
+import BurgerIngridient from "./Burger-Ingredient/BurgerIngredients";
 
-const burger = (props) => {
-    return (
-        <div className={classes.Burger}>
-        <BurgerIngridient type="bread-top"/>
-        <BurgerIngridient type="cheese"/>
-        <BurgerIngridient type="meat"/>
-        <BurgerIngridient type="bread-bottom"/>
-            
-        </div>
-    )
-}
+const burger = props => {
+  // Key return an array so we can use maps --- as Strings
+  const transformIngridients = Object.keys(props.ingredients)
+  .map(igKey => {
+      // spread operator ... create another array with the length of the value assaing to the ingredient
+      return [...Array(props.ingredients[igKey])]
+      .map((_,i) =>{
+        return <BurgerIngridient key={igKey + i} type={igKey}/>;
+      })
+  });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngridient type="bread-top" />
+      {transformIngridients}
+      <BurgerIngridient type="bread-bottom" />
+    </div>
+  );
+};
 
 export default burger;
