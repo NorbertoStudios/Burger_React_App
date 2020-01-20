e1fe9bbb2725b9085dae830b388b6830090b7618
Norberto Fernandez <Norberto@Norbertos-MacBook-Pro.local>
2020-01-20 02:33:49 -0500

    Added the controls css and rendered to the browser

--- burgerbuilder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burgerbuilder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -11,5 +11,4 @@ const buildControl = (props) => (
     );
 
 
-export default buildControl
-props
+export default buildControl;
--- burgerbuilder/src/components/Burger/BuildControls/BuildControls.js
+++ burgerbuilder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,20 @@
 import React from "react";
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
 
-import classes from './BuildControls.module.css'
+const controls = [
+  {label: "Salad", type: "salad"},
+  {label: "Bacon",type: "bacon"},
+  {label: "Meat",type: "meat"},
+  {label: "Cheese",type: "cheese"}
+];
 
-const buildControls = props => <div></div>;
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
+  </div>
+);
 
 export default buildControls;
